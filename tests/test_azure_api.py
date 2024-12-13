bb074a5555a7d328d187d6d1b8c23f4553db48fc
pallavp-metron <pallav.pandey@metronlabs.com>
2024-12-13 15:07:37 +0530

    Update test_azure_api.py

--- tests/test_azure_api.py
+++ tests/test_azure_api.py
@@ -2,10 +2,18 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
+import json
 from unittest.mock import patch, Mock
 from azure_api import fetch_sign_in_logs, get_access_token
 from datetime import datetime
-from tests.test_output.mock_data import mock_sign_in_response
+
+# Path of mock JSON file
+MOCK_DATA_PATH = os.path.join(os.path.dirname(__file__), "test_output", "mock_data.json")
+
+def load_mock_data():
+    """Load mock data from the JSON file."""
+    with open(MOCK_DATA_PATH, "r") as file:
+        return json.load(file)
 
 @patch("azure_api.requests.post")
 def test_get_access_token(mock_post):
@@ -17,7 +25,6 @@ def test_get_access_token(mock_post):
         "ext_expires_in": 3599,
         "access_token": "mock_token",
     }
-    mock_post
     mock_post.return_value = mock_response
 
     creds = {"tenant_id": "test_tenant", "client_id": "test_client", "client_secret": "test_secret"}
@@ -39,7 +46,7 @@ def test_fetch_sign_in_logs(mock_post, mock_get):
 
     mock_api_response = Mock()
     mock_api_response.status_code = 200
-    mock_api_response.json.return_value = mock_sign_in_response
+    mock_api_response.json.return_value = load_mock_data()
     mock_get.return_value = mock_api_response
 
     creds = {"tenant_id": "test_tenant", "client_id": "test_client", "client_secret": "test_secret"}
@@ -47,4 +54,4 @@ def test_fetch_sign_in_logs(mock_post, mock_get):
     end_date = datetime(2024, 12, 9)
 
     logs = fetch_sign_in_logs(creds, start_date, end_date)
-    assert logs == mock_sign_in_response
+    assert logs == load_mock_data()    
